9d7b382a64c28bc7d29ba0b70810729b5d997a35
Ashley Davis <ashley921davis@gmail.com>
2025-04-09 18:01:33 -0400

    Initial commit - copied from a6 branch

---
+++ Kambaz/Assignments/dao.js
@@ -0,0 +1,26 @@
+import { v4 as uuidv4 } from "uuid";
+import model from "./model.js"
+
+export function findAllAssignments() {
+    return model.find({});
+}
+export function findAssignmentById(assignmentId) {
+    return model.find({ _id: assignmentId })
+}
+
+export function findAssignmentByCourseId(courseId) {
+    return model.find({ course: courseId })
+}
+
+export function createAssignment(assignment) {
+    const newAssignment = { ...assignment, _id: uuidv4() };
+    return model.create(newAssignment);
+}
+
+export function updateAssignment(assignmentId, assignmentUpdates) {
+    return model.updateOne({ _id: assignmentId }, assignmentUpdates)
+}
+
+export function deleteAssignment(assignmentId) {
+    return model.deleteOne({ _id: assignmentId })
+}
---
+++ Kambaz/Modules/dao.js
@@ -0,0 +1,14 @@
+import model from "./model.js"
+export function findModulesForCourse(courseId) {
+    return model.find({ course: courseId });
+}
+export function createModule(module) {
+    const newModule = { ...module, _id: uuidv4() };
+    return model.create(newModule);
+}
+export function deleteModule(moduleId) {
+    return model.deleteOne({ _id: moduleId });
+}
+export function updateModule(moduleId, moduleUpdates) {
+    return model.updateOne({ _id: moduleId }, moduleUpdates);
+}
